37f7d5bacd12e0da01b29867b1731c9b49681005
Steve Boardwell <steve.boardwell@datameer.com>
2020-10-19 13:45:57 +0200

    Changes for 0.14.0

--- src/main/java/datameer/awstasks/util/Ec2Util.java
+++ src/main/java/datameer/awstasks/util/Ec2Util.java
@@ -47,7 +47,10 @@ public class Ec2Util {
 
     public static List<Instance> findByGroup(AmazonEC2 ec2, String securityGroup, boolean includeMultipleReservations, InstanceStateName... instanceStates) {
         List<Reservation> reservations = ec2.describeInstances(new DescribeInstancesRequest().withFilters(Filters.groupNameEc2Classic(securityGroup), Filters.instanceStates(instanceStates))).getReservations();
-        reservations.addAll(ec2.describeInstances(new DescribeInstancesRequest().withFilters(Filters.groupName(securityGroup), Filters.instanceStates(instanceStates))).getReservations());
+        if (reservations.isEmpty()) {
+            // couldn't find any groups in the classic way, attempting the new way
+            reservations.addAll(ec2.describeInstances(new DescribeInstancesRequest().withFilters(Filters.groupName(securityGroup), Filters.instanceStates(instanceStates))).getReservations());
+        }
         if (reservations.size() > 1 && !includeMultipleReservations) {
             throw new IllegalArgumentException("found more then one (" + reservations.size() + ") running instance groups (/reservations) for the given security group '" + securityGroup
                     + "' with instances in '" + Arrays.asList(instanceStates) + "' mode");
